c4d0231173d43633d3fec99d926510d5741bec0b
이병권 (베이직인터내셔널)(Lee Byeong Gwon)/DX)IT혁신팀(협력업체) <comlbg@ls-electric.com>
2026-07-08 10:24:48 +0900

    chore: MailService 로그 코드 반영 (ge-api 원격 드리프트 동기화)
    ge-api 원격에는 있었으나 앞서 subtree pull 시점 이후 추가되어
    로컬에는 누락되어 있던 log.info 로그 코드를 반영

--- src/main/java/com/ge/bo/common/mail/MailService.java
+++ src/main/java/com/ge/bo/common/mail/MailService.java
@@ -3,10 +3,12 @@ package com.ge.bo.common.mail;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class MailService {
@@ -14,6 +16,8 @@ public class MailService {
     private final JavaMailSender mailSender;
 
     public void sendMail(String to, String subject, String content) {
+
+        log.info("메일 전송 Start");
         try {
             MimeMessage message = mailSender.createMimeMessage();
 
@@ -26,8 +30,11 @@ public class MailService {
             mailSender.send(message);
 
         } catch (MessagingException e) {
+            log.info("메일 전송 실패");
+            log.info(e.getMessage());
             throw new RuntimeException("메일 전송 실패", e);
         }
+        log.info("메일 전송 End");
     }
 }
 
